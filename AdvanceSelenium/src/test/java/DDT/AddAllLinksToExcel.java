e51dec4a584d4b6f299a0e3b6ca01e47279af2b6
ShwetaH12 <shweta.zagade1209@gmail.com>
2024-09-11 20:52:07 +0530

    Update AddAllLinksToExcel.java

--- AdvanceSelenium/src/test/java/DDT/AddAllLinksToExcel.java
+++ AdvanceSelenium/src/test/java/DDT/AddAllLinksToExcel.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//pull
 public class AddAllLinksToExcel {
 
 	public static void main(String[] args) throws Throwable, IOException {
